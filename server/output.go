60f0ede93d99d791c8f557fdb53747fcb9ba9aef
Oklahomer <hagiwara.go@gmail.com>
2017-11-05 17:05:27 +0900

    Add types to represent outgoing messages

---
+++ server/output.go
@@ -0,0 +1,103 @@
+package server
+
+import (
+	"errors"
+
+	"github.com/oklahomer/go-sarah"
+	"github.com/oklahomer/go-sarah-iot"
+)
+
+// Destination represents what devices to send output.
+// Since not all fields are set on every occasion, this should be constructed with DestinationBuilder with proper validation.
+type Destination struct {
+	role     *iot.Role
+	deviceID string
+}
+
+var _ sarah.OutputDestination = (*Destination)(nil)
+
+// DestinationBuilder provides safe mechanism to build sarah.OutputDestination.
+type DestinationBuilder struct {
+	role     *iot.Role
+	deviceID string
+}
+
+// NewDestinationBuilder returns new DestinationBuilder.
+func NewDestinationBuilder() *DestinationBuilder {
+	return &DestinationBuilder{}
+}
+
+// Role is a setter to designate sending destination with given role.
+func (db *DestinationBuilder) Role(r *iot.Role) *DestinationBuilder {
+	db.role = r
+	return db
+}
+
+// DeviceID is a setter to designate sending destination with given device id.
+func (db *DestinationBuilder) DeviceID(id string) *DestinationBuilder {
+	db.deviceID = id
+	return db
+}
+
+// Build builds sarah.OutputDestination with preset arguments.
+func (db *DestinationBuilder) Build() (sarah.OutputDestination, error) {
+	if db.role == nil {
+		return nil, errors.New("*iot.Role must be set")
+	}
+
+	return &Destination{
+		role:     db.role,
+		deviceID: db.deviceID,
+	}, nil
+}
+
+type transactionalOutput struct {
+	transactionID string
+	c             chan []*Response
+	destination   *Destination
+	content       interface{}
+}
+
+var _ sarah.Output = (*transactionalOutput)(nil)
+
+// NewTransactionalOutput creates sarah.Output that represents a request for enslaved devices and returns it with communicating channel.
+func NewTransactionalOutput(transactionID string, destination *Destination, content interface{}) (sarah.Output, <-chan []*Response) {
+	c := make(chan []*Response)
+	return &transactionalOutput{
+		transactionID: transactionID,
+		c:             c,
+		destination:   destination,
+		content:       content,
+	}, c
+}
+
+func (to *transactionalOutput) Destination() sarah.OutputDestination {
+	return to.destination
+}
+
+func (to *transactionalOutput) Content() interface{} {
+	return to.content
+}
+
+type output struct {
+	destination *Destination
+	content     interface{}
+}
+
+var _ sarah.Output = (*output)(nil)
+
+func (o *output) Destination() sarah.OutputDestination {
+	return o.destination
+}
+
+func (o *output) Content() interface{} {
+	return o.content
+}
+
+// NewOutput creates sarah.Output that is to be sent to given destination.
+func NewOutput(destination *Destination, content interface{}) sarah.Output {
+	return &output{
+		destination: destination,
+		content:     content,
+	}
+}
---
+++ server/output_test.go
@@ -0,0 +1,167 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/oklahomer/go-sarah-iot"
+)
+
+func TestNewOutput(t *testing.T) {
+	destination := &Destination{}
+	content := struct{}{}
+
+	o := NewOutput(destination, content)
+
+	if o == nil {
+		t.Fatalf("Returned Output is nil.")
+	}
+
+	typed, ok := o.(*output)
+	if !(ok) {
+		t.Fatalf("Given sarah.Output is not *output: %T.", o)
+	}
+
+	if typed.destination != destination {
+		t.Errorf("Invalid destination is set: %+v.", typed.destination)
+	}
+
+	if typed.content != content {
+		t.Errorf("Invalid content is set: %+v.", typed.content)
+	}
+}
+
+func TestOutput_Destination(t *testing.T) {
+	destination := &Destination{}
+	o := &output{
+		destination: destination,
+	}
+
+	if o.Destination() != destination {
+		t.Fatalf("Unexpected sarah.Destination is returned: %T.", o.Destination())
+	}
+}
+
+func TestOutput_Content(t *testing.T) {
+	content := struct{}{}
+	o := &output{
+		content: content,
+	}
+
+	if o.Content() != content {
+		t.Fatalf("Unexpected content is returned: %T.", o.Content())
+	}
+}
+
+func TestNewTransactionalOutput(t *testing.T) {
+	tid := "123"
+	destination := &Destination{}
+	content := struct{}{}
+
+	o, _ := NewTransactionalOutput(tid, destination, content)
+
+	if o == nil {
+		t.Fatalf("Returned Output is nil.")
+	}
+
+	typed, ok := o.(*transactionalOutput)
+	if !(ok) {
+		t.Fatalf("Given sarah.Output is not *output: %T.", o)
+	}
+
+	if typed.destination != destination {
+		t.Errorf("Invalid destination is set: %+v.", typed.destination)
+	}
+
+	if typed.content != content {
+		t.Errorf("Invalid content is set: %+v.", typed.content)
+	}
+
+	if typed.transactionID != tid {
+		t.Errorf("Invalid transaction id is set: %s.", typed.transactionID)
+	}
+}
+
+func TestTransactionalOutput_Destination(t *testing.T) {
+	destination := &Destination{}
+	o := &transactionalOutput{
+		destination: destination,
+	}
+
+	if o.Destination() != destination {
+		t.Fatalf("Unexpected sarah.Destination is returned: %T.", o.Destination())
+	}
+}
+
+func TestTransactionalOutput_Content(t *testing.T) {
+	content := struct{}{}
+	o := &transactionalOutput{
+		content: content,
+	}
+
+	if o.Content() != content {
+		t.Fatalf("Unexpected content is returned: %T.", o.Content())
+	}
+}
+
+func TestNewDestinationBuilder(t *testing.T) {
+	builder := NewDestinationBuilder()
+
+	if builder == nil {
+		t.Errorf("Nil is returned.")
+	}
+}
+
+func TestDestinationBuilder_Role(t *testing.T) {
+	role := iot.NewRole("role")
+	builder := &DestinationBuilder{}
+
+	builder.Role(role)
+
+	if builder.role != role {
+		t.Errorf("Given *iot.Role is not set: %+v.", builder.role)
+	}
+}
+
+func TestDestinationBuilder_DeviceID(t *testing.T) {
+	deviceID := "id"
+	builder := &DestinationBuilder{}
+
+	builder.DeviceID(deviceID)
+
+	if builder.deviceID != deviceID {
+		t.Errorf("Given device ID is not set: %s.", builder.deviceID)
+	}
+}
+
+func TestDestinationBuilder_Build(t *testing.T) {
+	deviceID := "123"
+	builder := &DestinationBuilder{
+		deviceID: deviceID,
+	}
+	destination, err := builder.Build()
+	if err == nil {
+		t.Fatalf("Error should be returned when *iot.Role is not set.")
+	}
+
+	role := iot.NewRole("dummy")
+	builder.role = role
+	destination, err = builder.Build()
+
+	if err != nil {
+		t.Fatalf("Unexpected error is returned: %s.", err.Error())
+	}
+
+	typed, ok := destination.(*Destination)
+
+	if !ok {
+		t.Fatalf("Returned sarah.OutputDestination is not instance of Output: %T.", destination)
+	}
+
+	if typed.deviceID != deviceID {
+		t.Errorf("Given device id is not set: %s.", typed.deviceID)
+	}
+
+	if typed.role != role {
+		t.Errorf("Given *iot.Role is not set: %+v.", typed.role)
+	}
+}
