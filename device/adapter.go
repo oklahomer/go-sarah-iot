8eb25eb5ff1a809ae0d695740113f64fcb7071d6
Oklahomer <hagiwara.go@gmail.com>
2017-11-12 12:01:25 +0900

    Add device adapter.

---
+++ device/adapter.go
@@ -0,0 +1,415 @@
+package device
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/oklahomer/go-sarah"
+	"github.com/oklahomer/go-sarah-iot"
+	"github.com/oklahomer/go-sarah-iot/payload"
+	"github.com/oklahomer/go-sarah/log"
+	"github.com/oklahomer/go-sarah/retry"
+	"golang.org/x/net/context"
+)
+
+var (
+	// ConnectedOutputDestination can be used as sarah.OutputDestination instead of assigning specific value each time.
+	//
+	// In this Adapter, outgoing messages are only sent to central server which is connected via WebSocket connection.
+	// This messaging typically does not need any additional information such as "talk room" or "recipient user"
+	// so assigning sarah.OutputDestination does not really make sense on regular use case.
+	//
+	// However sarah.Runner obligates sarah.ScheduledTask.Execute() to return non-nil sarah.OutputDestination
+	// or obligates sarah.ScheduledTask.DefaultDestination to be non-nil.
+	ConnectedOutputDestination = struct{}{}
+)
+
+const (
+	// IotDevice is a designated sarah.BotType for iot device.
+	IoTDevice sarah.BotType = "iot-device"
+)
+
+// Config contains some configuration variables for iot device Adapter.
+type Config struct {
+	ServerURL        string `json:"server_url" yaml:"server_url"`
+	SendingQueueSize uint   `json:"sending_queue_size" yaml:"sending_queue_size"`
+}
+
+// NewConfig returns pointer to Config struct with default settings.
+// To override each field values, pass this instance to jsono.Unmarshal/yaml.Unmarshal or assign directly.
+func NewConfig() *Config {
+	return &Config{
+		ServerURL:        "",
+		SendingQueueSize: 100,
+	}
+}
+
+// AdapterOption defines function signature that Adapter's functional option must satisfy.
+type AdapterOption func(*adapter) error
+
+// WithDecoder creates AdapterOption with given payload.Decoder implementation.
+func WithDecoder(decoder payload.Decoder) AdapterOption {
+	return func(a *adapter) error {
+		a.decoder = decoder
+		return nil
+	}
+}
+
+// WithEncoder creates AdapterOption with given payload.Encoder implementation.
+func WithEncoder(encoder payload.Encoder) AdapterOption {
+	return func(a *adapter) error {
+		a.encoder = encoder
+		return nil
+	}
+}
+
+type adapter struct {
+	config  *Config
+	decoder payload.Decoder
+	encoder payload.Encoder
+	output  chan *outputContent
+}
+
+var _ sarah.Adapter = (*adapter)(nil)
+
+// BotType returns sarah.BotType of this particular instance.
+func (a *adapter) BotType() sarah.BotType {
+	return IoTDevice
+}
+
+// Run establishes connection with configured server and starts interaction.
+func (a *adapter) Run(ctx context.Context, enqueueInput func(sarah.Input) error, notifyErr func(error)) {
+	for {
+		conn, err := a.connect()
+		if err != nil {
+			notifyErr(sarah.NewBotNonContinuableError(err.Error()))
+			return
+		}
+
+		connErr := a.superviseConnection(ctx, conn, enqueueInput)
+		conn.Close() // Make sure to close when connection is no longer supervised.
+		if connErr == nil {
+			// Connection is intentionally closed by caller with context cancellation.
+			// No more interaction follows.
+			return
+		}
+
+		log.Errorf("Will try re-connection due to previous connection's fatal state: %s.", connErr.Error())
+	}
+}
+
+// SendMessage let Bot send message to connected server.
+func (a *adapter) SendMessage(ctx context.Context, o sarah.Output) {
+	// Loosely validate type before enqueue.
+	switch typed := o.Content().(type) {
+	case *outputContent:
+		a.output <- typed
+
+	default:
+		log.Errorf("Uncontrollable output type is passed: %T", typed)
+
+	}
+}
+
+// NewAdapter creates new Adapter with given *Config and arbitrary amount of AdapterOption instances.
+func NewAdapter(config *Config, options ...AdapterOption) (sarah.Adapter, error) {
+	a := &adapter{
+		config: config,
+		output: make(chan *outputContent, config.SendingQueueSize),
+	}
+
+	for _, opt := range options {
+		err := opt(a)
+		if err != nil {
+			return nil, fmt.Errorf("failed to execute AdapterOption: %s", err.Error())
+		}
+	}
+
+	// If no payload.Decoder is provided via AdapterOption, set default decoder.
+	if a.decoder == nil {
+		a.decoder = payload.NewDecoder()
+	}
+
+	// If no payload.Encoder is provided via AdapterOption, set default encoder.
+	if a.encoder == nil {
+		a.encoder = payload.NewEncoder()
+	}
+
+	return a, nil
+}
+
+func (a *adapter) connect() (connection, error) {
+	url := a.config.ServerURL
+	if url == "" {
+		return nil, errors.New("empty url is set to Config.ServerURL")
+	}
+
+	var conn *websocket.Conn
+	err := retry.WithInterval(10, func() (e error) {
+		// TODO set header values for authorization
+		header := http.Header{}
+
+		dialer := websocket.Dialer{}
+		conn, _, e = dialer.Dial(url, header)
+
+		return
+	}, 3*time.Second)
+
+	if err != nil {
+		return nil, err
+	}
+
+	conn.SetPongHandler(func(appData string) error {
+		log.Debugf("Received Pong: %s", appData)
+		return nil
+	})
+
+	return &connWrapper{
+		conn: conn,
+	}, err
+}
+
+func (a *adapter) receivePayload(conn connection, enqueueInput func(sarah.Input) error) error {
+	for {
+		// DO NOT try ping or other alternative method to check connection status.
+		// See github.com/gorilla/websocket's document for Conn.NextReader.
+		//
+		// "Applications must break out of the application's read loop when this method
+		// returns a non-nil error value. Errors returned from this method are
+		// permanent. Once this method returns a non-nil error, all subsequent calls to
+		// this method return the same error."
+		messageType, reader, err := conn.NextReader()
+		if err != nil {
+			if err != nil {
+				return fmt.Errorf("failed to read incoming message: %s", err.Error())
+			}
+		}
+
+		switch messageType {
+		case websocket.TextMessage, websocket.BinaryMessage:
+			p, err := a.decoder.Decode(messageType, reader)
+			if err != nil {
+				log.Errorf("Failed to decode received payload: %s", err.Error())
+				continue
+			}
+
+			switch typed := p.(type) {
+			case *payload.RoledPayload:
+				enqueueInput(&Input{
+					Sender:    a.config.ServerURL,
+					Content:   typed.Content,
+					TimeStamp: time.Now(), // TODO
+				})
+
+			case *payload.TransactionalPayload:
+				enqueueInput(&TransactionalInput{
+					TransactionID: typed.TransactionID,
+					Sender:        a.config.ServerURL,
+					Content:       typed.Content,
+					TimeStamp:     time.Now(), // TODO
+				})
+
+			default:
+				log.Errorf("Uncontrollable payload type is returned: %+v", p)
+				continue
+
+			}
+
+		default:
+			log.Errorf("Unknown message type is returned: %d.", messageType)
+
+		}
+	}
+}
+
+func (a *adapter) superviseConnection(connCtx context.Context, conn connection, enqueueInput func(sarah.Input) error) error {
+	pingTicker := time.NewTicker(30 * time.Second) // TODO set via Config
+	defer pingTicker.Stop()
+
+	permanentReadErr := make(chan error, 1)
+	go func() {
+		err := a.receivePayload(conn, enqueueInput)
+		if err != nil {
+			permanentReadErr <- err
+		}
+	}()
+
+	for {
+		select {
+		case <-connCtx.Done():
+			return nil
+
+		case err := <-permanentReadErr:
+			return err
+
+		case message := <-a.output:
+			messageType, b, err := encodeOutput(message, a.encoder)
+			if err != nil {
+				log.Errorf("Failed to encode outgoing message: %s. %+v.", message, err.Error())
+				continue
+			}
+
+			switch messageType {
+			case websocket.TextMessage, websocket.BinaryMessage:
+				err = conn.WriteMessage(messageType, b)
+				if err != nil {
+					log.Warnf("Failed to send message: %s.", err.Error())
+					// When messaging fails, send PING immediately
+					// to make sure ping is sent before following messages are processed.
+					err = conn.WriteMessage(websocket.PingMessage, []byte{})
+					if err != nil {
+						return fmt.Errorf("error on ping: %s", err.Error())
+					}
+				}
+
+			default:
+				log.Errorf("Unsupported message type is given: %d. %+v.", messageType, message)
+
+			}
+
+		case <-pingTicker.C:
+			log.Debug("Send ping")
+			err := conn.WriteMessage(websocket.PingMessage, []byte{})
+			if err != nil {
+				return fmt.Errorf("error on ping: %s", err.Error())
+			}
+
+		}
+	}
+}
+
+func encodeOutput(o *outputContent, e payload.Encoder) (int, []byte, error) {
+	if o.transactionID == "" {
+		return e.EncodeRoled(o.role, o.payloadContent)
+	}
+
+	return e.EncodeTransactional(o.transactionID, o.role, o.payloadContent)
+}
+
+type connection interface {
+	Close() error
+	NextReader() (int, io.Reader, error)
+	WriteMessage(int, []byte) error
+	Pong() error
+}
+
+type connWrapper struct {
+	conn *websocket.Conn
+}
+
+var _ connection = (*connWrapper)(nil)
+
+func (cw *connWrapper) Close() error {
+	return cw.conn.Close()
+}
+
+func (cw *connWrapper) NextReader() (int, io.Reader, error) {
+	return cw.conn.NextReader()
+}
+
+func (cw *connWrapper) WriteMessage(messageType int, payload []byte) error {
+	return cw.conn.WriteMessage(messageType, payload)
+}
+
+func (cw *connWrapper) Pong() error {
+	return cw.conn.WriteMessage(websocket.PongMessage, []byte{})
+}
+
+type Input struct {
+	Sender    string
+	Content   interface{}
+	TimeStamp time.Time
+}
+
+var _ sarah.Input = (*Input)(nil)
+
+func (i Input) SenderKey() string {
+	return i.Sender
+}
+
+func (i *Input) Message() string {
+	return ""
+}
+
+func (i *Input) SentAt() time.Time {
+	return i.TimeStamp
+}
+
+func (i *Input) ReplyTo() sarah.OutputDestination {
+	return ConnectedOutputDestination
+}
+
+type TransactionalInput struct {
+	TransactionID string
+	Sender        string
+	Content       interface{}
+	TimeStamp     time.Time
+}
+
+var _ sarah.Input = (*TransactionalInput)(nil)
+
+func (i TransactionalInput) SenderKey() string {
+	return i.Sender
+}
+
+func (i *TransactionalInput) Message() string {
+	return ""
+}
+
+func (i *TransactionalInput) SentAt() time.Time {
+	return i.TimeStamp
+}
+
+func (i *TransactionalInput) ReplyTo() sarah.OutputDestination {
+	return ConnectedOutputDestination
+}
+
+// NewResponse creates *sarah.CommandResponse with given iot.Role and payload content.
+// This method is seldom used because usually iot.server initiates requests and device responds,
+// which means transaction id is usually given as part of input and is to be used as part of response.
+// NewTransactionResponse should be used for such case.
+func NewResponse(role *iot.Role, payloadContent interface{}) *sarah.CommandResponse {
+	return &sarah.CommandResponse{
+		UserContext: nil,
+		Content: &outputContent{
+			role:           role,
+			payloadContent: payloadContent,
+		},
+	}
+}
+
+// NewTransactionResponse can be used to build sarah.CommandResponse with given clues.
+// When this is returned by sarah.Command, this object is internally used to construct responding payload.
+func NewTransactionResponse(transactionID string, role *iot.Role, payloadContent interface{}) *sarah.CommandResponse {
+	return &sarah.CommandResponse{
+		UserContext: nil,
+		Content: &outputContent{
+			transactionID:  transactionID,
+			role:           role,
+			payloadContent: payloadContent,
+		},
+	}
+}
+
+// NewOutput constructs sarah.Output with given arguments.
+// This can be used as part of returning value of ScheduledTask.
+func NewOutput(role *iot.Role, payloadContent interface{}) sarah.Output {
+	return sarah.NewOutputMessage(
+		ConnectedOutputDestination,
+		&outputContent{
+			role:           role,
+			payloadContent: payloadContent,
+		})
+}
+
+type outputContent struct {
+	role           *iot.Role
+	payloadContent interface{}
+
+	// This field is set if and only if the outgoing payload represents a transactional output, which is a response to given request.
+	transactionID string
+}
---
+++ device/adapter_test.go
@@ -0,0 +1,390 @@
+package device
+
+import (
+	"context"
+	"errors"
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/oklahomer/go-sarah"
+	"github.com/oklahomer/go-sarah-iot"
+	"github.com/oklahomer/go-sarah-iot/payload"
+)
+
+type DummyConnection struct {
+	CloseFunc        func() error
+	NextReaderFunc   func() (int, io.Reader, error)
+	WriteMessageFunc func(int, []byte) error
+	PongFunc         func() error
+}
+
+func (d *DummyConnection) Close() error {
+	return d.CloseFunc()
+}
+
+func (d *DummyConnection) NextReader() (int, io.Reader, error) {
+	return d.NextReaderFunc()
+}
+
+func (d *DummyConnection) WriteMessage(messageType int, output []byte) error {
+	return d.WriteMessageFunc(messageType, output)
+}
+
+func (d *DummyConnection) Pong() error {
+	return d.PongFunc()
+}
+
+type DummyEncoder struct {
+	EncodeFunc              func(string, interface{}) (int, []byte, error)
+	EncodeRoledFunc         func(*iot.Role, interface{}) (int, []byte, error)
+	EncodeTransactionalFunc func(string, *iot.Role, interface{}) (int, []byte, error)
+}
+
+func (de *DummyEncoder) Encode(payloadType string, content interface{}) (int, []byte, error) {
+	return de.EncodeFunc(payloadType, content)
+}
+
+func (de *DummyEncoder) EncodeRoled(role *iot.Role, content interface{}) (int, []byte, error) {
+	return de.EncodeRoledFunc(role, content)
+}
+
+func (de *DummyEncoder) EncodeTransactional(tid string, role *iot.Role, content interface{}) (int, []byte, error) {
+	return de.EncodeTransactionalFunc(tid, role, content)
+}
+
+type DummyDecoder struct {
+	AddMappingFunc func(*iot.Role, reflect.Type)
+	DecodeFunc     func(int, io.Reader) (payload.DecodedPayload, error)
+}
+
+func (d *DummyDecoder) AddMapping(r *iot.Role, t reflect.Type) {
+	d.AddMappingFunc(r, t)
+}
+
+func (d *DummyDecoder) Decode(messageType int, reader io.Reader) (payload.DecodedPayload, error) {
+	return d.DecodeFunc(messageType, reader)
+}
+
+func TestNewConfig(t *testing.T) {
+	c := NewConfig()
+
+	if c == nil {
+		t.Fatal("Pointer to Config is not returned.")
+	}
+}
+
+func TestWithDecoder(t *testing.T) {
+	decoder := &DummyDecoder{}
+	opt := WithDecoder(decoder)
+	adapter := &adapter{}
+
+	err := opt(adapter)
+
+	if err != nil {
+		t.Fatalf("AdapterOption returned an error: %s.", err.Error)
+	}
+
+	if adapter.decoder != decoder {
+		t.Fatalf("Given payload.Decoder is not set: %+v", adapter.decoder)
+	}
+}
+
+func TestWithEncoder(t *testing.T) {
+	encoder := &DummyEncoder{}
+	opt := WithEncoder(encoder)
+	adapter := &adapter{}
+
+	err := opt(adapter)
+
+	if err != nil {
+		t.Fatalf("AdapterOption returned an error: %s.", err.Error)
+	}
+
+	if adapter.encoder != encoder {
+		t.Fatalf("Given payload.Encoder is not set: %+v", adapter.encoder)
+	}
+}
+
+func TestNewAdapter(t *testing.T) {
+	c := &Config{}
+	a, err := NewAdapter(c)
+
+	if err != nil {
+		t.Fatalf("Failed to initialize adapter: %s", err.Error())
+	}
+
+	typed, ok := a.(*adapter)
+	if !ok {
+		t.Fatalf("Pointer to adapter is not returned: %T", a)
+	}
+
+	if typed.config != c {
+		t.Errorf("Given *Config is not set: %+v", typed.config)
+	}
+
+	if typed.decoder == nil {
+		t.Errorf("Default payload.Decoder must be set when one is not supplied with AdapterOption.")
+	}
+}
+
+func TestNewAdapter_WithAdapterOption(t *testing.T) {
+	decoder := &DummyDecoder{}
+	a, err := NewAdapter(&Config{}, WithDecoder(decoder))
+
+	if err != nil {
+		t.Fatalf("Failed to initialize adapter: %s", err.Error())
+	}
+
+	typed, ok := a.(*adapter)
+	if !ok {
+		t.Fatalf("Pointer to adapter is not returned: %T", a)
+	}
+
+	if typed.decoder != decoder {
+		t.Errorf("Given payload.Decoder is not set: %+v", typed.decoder)
+	}
+}
+
+func TestNewAdapter_WithErrorProneAdapterOption(t *testing.T) {
+	errishOption := func(_ *adapter) error {
+		return errors.New("boo")
+	}
+	_, err := NewAdapter(&Config{}, errishOption)
+
+	if err == nil {
+		t.Fatalf("Expected error is not returned.")
+	}
+}
+
+func TestAdapter_BotType(t *testing.T) {
+	a := &adapter{}
+	if a.BotType() != IoTDevice {
+		t.Errorf("Expected BotType is not returned: %s.", a.BotType())
+	}
+}
+
+func TestAdapter_SendMessage(t *testing.T) {
+	outputs := []sarah.Output{
+		sarah.NewOutputMessage(struct{}{}, struct{}{}), // invalid
+		NewOutput(iot.NewRole("dummy"), &outputContent{}),
+	}
+	a := &adapter{
+		output: make(chan *outputContent, len(outputs)),
+	}
+
+	for _, output := range outputs {
+		a.SendMessage(context.TODO(), output)
+	}
+
+	if len(a.output) != 1 {
+		t.Fatalf("Unexpected size of paylaods are queued: %d.", len(a.output))
+	}
+}
+
+func Test_receivePayload(t *testing.T) {
+	tests := []struct {
+		Payload payload.DecodedPayload
+		Reader  io.Reader
+		Type    reflect.Type
+	}{
+		{
+			Payload: &payload.RoledPayload{
+				Content: struct{}{},
+			},
+			Reader: strings.NewReader(`{"type": "roled", "role": "foo", "content": {}}`),
+			Type:   reflect.TypeOf(&Input{}),
+		},
+		{
+			Payload: &payload.TransactionalPayload{
+				TransactionID: "123",
+				Content:       struct{}{},
+			},
+			Reader: strings.NewReader(`{"type": "roled", "role": "foo", "transaction_id": "123", "content": {}}`),
+			Type:   reflect.TypeOf(&TransactionalInput{}),
+		},
+	}
+
+	for i, test := range tests {
+		i++
+
+		inputs := make(chan sarah.Input, 1)
+		input := func(i sarah.Input) error {
+			inputs <- i
+			return nil
+		}
+		decoder := &DummyDecoder{
+			DecodeFunc: func(_ int, _ io.Reader) (payload.DecodedPayload, error) {
+				return test.Payload, nil
+			},
+		}
+		a := &adapter{
+			decoder: decoder,
+			config: &Config{
+				ServerURL: "ws://localhost/dummy",
+			},
+		}
+
+		conn := &DummyConnection{
+			NextReaderFunc: func() (int, io.Reader, error) {
+				return websocket.TextMessage, test.Reader, nil
+			},
+		}
+
+		go a.receivePayload(conn, input)
+
+		<-time.NewTimer(100 * time.Millisecond).C
+
+		select {
+		case in := <-inputs:
+			if reflect.TypeOf(in) != test.Type {
+				t.Errorf("Expected type of payload is not returned on test #%d. Get %#v.", i, in)
+			}
+
+		default:
+			t.Errorf("Expected input is not arrived.")
+
+		}
+	}
+}
+
+func TestNewOutput(t *testing.T) {
+	role := iot.NewRole("dummy")
+	content := struct{}{}
+
+	output := NewOutput(role, content)
+
+	if output == nil {
+		t.Fatal("Expected sarah.Output instance is not returned.")
+	}
+
+	typed, ok := output.Content().(*outputContent)
+	if !ok {
+		t.Fatalf("Expected type of content is not set: %T.", typed)
+	}
+
+	if typed.role != role {
+		t.Errorf("Expected *iot.Role is not set: %+v", typed.role)
+	}
+
+	if typed.payloadContent != content {
+		t.Errorf("Expected content is not set: %+v", typed.payloadContent)
+	}
+
+	if typed.transactionID != "" {
+		t.Errorf("Unexpected transaction id is set: %s.", typed.transactionID)
+	}
+}
+
+func TestNewResponse(t *testing.T) {
+	role := iot.NewRole("dummy")
+	content := struct{}{}
+
+	response := NewResponse(role, content)
+
+	if response == nil {
+		t.Fatal("Expected *sarah.CommandResponse is not returned.")
+	}
+
+	if response.UserContext != nil {
+		t.Fatalf("Unexpected *sarah.UserContext is set: %+v.", response.UserContext)
+	}
+
+	typed, ok := response.Content.(*outputContent)
+	if !ok {
+		t.Fatalf("Expected type of content is not set: %T.", typed)
+	}
+
+	if typed.role != role {
+		t.Errorf("Expected *iot.Role is not set: %+v", typed.role)
+	}
+
+	if typed.payloadContent != content {
+		t.Errorf("Expected content is not set: %+v", typed.payloadContent)
+	}
+
+	if typed.transactionID != "" {
+		t.Errorf("Unexpected transaction id is set: %s.", typed.transactionID)
+	}
+}
+
+func TestNewTransactionResponse(t *testing.T) {
+	transactionID := "id"
+	role := iot.NewRole("dummy")
+	content := struct{}{}
+
+	response := NewTransactionResponse(transactionID, role, content)
+
+	if response == nil {
+		t.Fatal("Expected *sarah.CommandResponse is not returned.")
+	}
+
+	if response.UserContext != nil {
+		t.Fatalf("Unexpected *sarah.UserContext is set: %+v.", response.UserContext)
+	}
+
+	typed, ok := response.Content.(*outputContent)
+	if !ok {
+		t.Fatalf("Expected type of content is not set: %T.", typed)
+	}
+
+	if typed.role != role {
+		t.Errorf("Expected *iot.Role is not set: %+v", typed.role)
+	}
+
+	if typed.payloadContent != content {
+		t.Errorf("Expected content is not set: %+v", typed.payloadContent)
+	}
+
+	if typed.transactionID == "" {
+		t.Error("Expected transaction id is not set.")
+	}
+}
+
+func Test_encodeOutput(t *testing.T) {
+	outputs := []*outputContent{
+		{
+			role:           &iot.Role{},
+			payloadContent: struct{}{},
+		},
+		{
+			role:           &iot.Role{},
+			payloadContent: struct{}{},
+			transactionID:  "dummy",
+		},
+	}
+
+	for i, out := range outputs {
+		roledCalled := false
+		transactionalCalled := false
+		encoder := &DummyEncoder{
+			EncodeRoledFunc: func(_ *iot.Role, _ interface{}) (int, []byte, error) {
+				roledCalled = true
+				return websocket.TextMessage, []byte(""), nil
+			},
+			EncodeTransactionalFunc: func(_ string, _ *iot.Role, _ interface{}) (int, []byte, error) {
+				transactionalCalled = true
+				return websocket.TextMessage, []byte(""), nil
+			},
+		}
+
+		_, _, err := encodeOutput(out, encoder)
+
+		if err != nil {
+			t.Fatalf("Unexpected error is returned: %s.", err.Error())
+		}
+
+		if out.transactionID == "" {
+			if !roledCalled {
+				t.Errorf("Encoder.EncodeRolled is not called on test #%d.", i)
+			}
+		} else {
+			if !transactionalCalled {
+				t.Errorf("Encoder.EncodeTransactional is not called on test #%d.", i)
+			}
+		}
+	}
+}
